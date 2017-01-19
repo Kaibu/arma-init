71af81cb311c1c23bf950e13517699c8beccfd3b
Kaibu <Whocaresaboutgreeny@web.de>
2017-01-20 00:52:33 +0100

    init

---
+++ app/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+angular.module('myApp', [
+  'ngRoute',
+  'myApp.home',
+  'myApp.version'
+]).
+config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+  $locationProvider.hashPrefix('!');
+
+  $routeProvider.otherwise({redirectTo: '/home'});
+}]);
